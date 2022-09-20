ea30478224a09557309269b3f2074c1889c86ae7
PingHuskar <ping47024@icloud.com>
2022-09-20 10:35:14 +0700

    add Base64 encoding

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 const searchParam = new URLSearchParams(location.search)
 for (var shipNum = 1; shipNum <= 4; shipNum++) {
     document.getElementsByClassName('container')[0].innerHTML += `<div class="ship" id="ship${shipNum}" contenteditable oninput="main();" onkeyup="animateShip(this.id)"></div>`
-    document.getElementById(`ship${shipNum}`).innerText = searchParam.get(`ship${shipNum}`)
+    document.getElementById(`ship${shipNum}`).innerText = atob(searchParam.get(`ship${shipNum}`))
 }
 TweenMax.staggerFrom('.ship',1,{
-    x: 2500,autoAlpha:0,ease:Linear.easeNone
+    x: screen.width,autoAlpha:0,ease:Linear.easeNone
 },0.1)
 
 const animateShip = (shipId) => {
-    console.log(`#${shipId}`)
+    // console.log(`#${shipId}`)
     TweenMax.fromTo(`#${shipId}`,1,{
         x: 0,ease:Linear.easeNone
     },{
-        x: -1500,ease:Linear.easeNone
+        x: -screen.width*2,ease:Linear.easeNone
     })
     TweenMax.fromTo(`#${shipId}`,1,{
-        x: 1500,ease:Linear.easeNone,delay:1.5
+        x: screen.width*2,ease:Linear.easeNone,delay:0.3
     },{
-        x: 0,ease:Linear.easeNone,delay:1.5
+        x: 0,ease:Linear.easeNone,delay:0.3
     })
 }
 
@@ -27,10 +27,10 @@ const main = () => {
     for (var shipNum = 1; shipNum <= 4; shipNum++) {
         var shipVal = document.getElementById(`ship${shipNum}`).innerText
         sum.push(parseInt(shipVal) || 0)
-        url.searchParams.set(`ship${shipNum}`, shipVal)
+        url.searchParams.set(`ship${shipNum}`, btoa(shipVal))
     }
     const cal = sum.join(" + ")
     console.log(`${cal} = ${eval(cal)}`)
-    url.searchParams.set(`res`, eval(cal))
+    url.searchParams.set(`res`, btoa(eval(cal)))
     history.pushState({}, '', url);
 }
