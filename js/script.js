f4d104f0af64238321c6cd97aefdb1bbd022804a
Chadin Chaipornpisuth <ping47024@icloud.com>
2023-03-22 11:29:42 +0700

    v1

--- js/script.js
+++ js/script.js
@@ -1,42 +1,22 @@
-const searchParam = new URLSearchParams(location.search)
+
 for (var shipNum = 1; shipNum <= 4; shipNum++) {
-    document.getElementsByClassName('container')[0].innerHTML += `<div class="ship" id="ship${shipNum}" contenteditable oninput="main();" onkeyup="animateShip(this.id)"></div>`
-    var shipVal = searchParam.get(`ship${shipNum}`)
-    if (shipVal !== null) {
-        document.getElementById(`ship${shipNum}`).innerText = atob(shipVal)
-    }
-    else {
-        document.getElementById(`ship${shipNum}`).innerText = 0
-    }
+    document.getElementsByClassName('container')[0].innerHTML += `<div class="ship" id="ship${shipNum}" contenteditable onblur="animateShip(this)">${localStorage.getItem("ship"+shipNum) || 0}</div>`
 }
 TweenMax.staggerFrom('.ship',1,{
     x: screen.width,autoAlpha:0,ease:Linear.easeNone
 },0.1)
 
-const animateShip = (shipId) => {
+const animateShip = (ship) => {
     // console.log(`#${shipId}`)
-    TweenMax.fromTo(`#${shipId}`,1,{
+    localStorage.setItem(`${ship.id}`,ship.innerText)
+    TweenMax.fromTo(`#${ship.id}`,1,{
         x: 0,ease:Linear.easeNone
     },{
         x: -screen.width*2,ease:Linear.easeNone
     })
-    TweenMax.fromTo(`#${shipId}`,1,{
+    TweenMax.fromTo(`#${ship.id}`,1,{
         x: screen.width*2,ease:Linear.easeNone,delay:0.3
     },{
         x: 0,ease:Linear.easeNone,delay:0.3
     })
-}
-
-const main = () => {
-    const url = new URL(location)
-    var sum = []
-    for (var shipNum = 1; shipNum <= 4; shipNum++) {
-        var shipVal = document.getElementById(`ship${shipNum}`).innerText
-        sum.push(parseInt(shipVal) || 0)
-        url.searchParams.set(`ship${shipNum}`, btoa(shipVal))
-    }
-    const cal = sum.join(" + ")
-    console.log(`${cal} = ${eval(cal)}`)
-    url.searchParams.set(`res`, btoa(eval(cal)))
-    history.pushState({}, '', url);
 }
